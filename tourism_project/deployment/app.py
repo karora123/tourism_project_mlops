61ef6f584d1e52c63dee4a63aa687c7b837b666b
kulbhushan <kulbhushan.arora85@gmail.com>
2025-10-05 16:29:17 +0530

    Update app.py
    repo name correction

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download and load the model
-model_path = hf_hub_download(repo_id="karora1804/tourism-project-gl", filename="best_tourism_package_model_v1.joblib")
+model_path = hf_hub_download(repo_id="karora1804/tourism-project-model", filename="best_tourism_package_model_v1.joblib")
 model = joblib.load(model_path)
 
 # Streamlit UI for Machine Failure Prediction
