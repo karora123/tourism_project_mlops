1eeca300a104864b7aef0f5692f2b0e53898b677
kulbhushan <kulbhushan.arora85@gmail.com>
2025-10-05 18:31:20 +0530

    Update hosting.py
    provided missing space_sdk="streamlit"

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -14,7 +14,7 @@ try:
     print(f"Space '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False, space_sdk="streamlit")
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
